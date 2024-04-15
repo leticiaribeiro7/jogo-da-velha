bc676df235606b80d48f3ac59e592227f9673253
leticiaribeiro7 <letsribeiro2@gmail.com>
2024-04-14 22:31:28 -0300

    using ansi to clear line

--- mouse.c
+++ mouse.c
@@ -28,20 +28,20 @@ int main() {
         y -= (int)(char)mouse_buffer[2]; // Movimento vertical
 
 
-        // Verificar o estado dos botões
+        // É verdadeiro quando o botão esquerdo tá pressionado,
+        // olhando apenas o bit ligado ao botao esquerdo (LSB)
         int left_button = mouse_buffer[0] & 0x01;
 
         
-            // Limpar a linha anterior imprimindo espaços
-            printf("\r                                                                                ");
-            
-            if (left_button == 1) {
-                printf("Clicou em x=%d e y=%d\n", x, y);
-            }
+        // Pra atualizar as coordenadas limpando e printando na mesma linha
+        printf("\r\033[K");
+        
+        if (left_button == 1) {
+            printf("Clicou em x=%d e y=%d\n", x, y);
+        }
 
-            // Exemplo de saída para demonstração
-            printf("\rPosição X: %d, Posição Y: %d, Botões: Left=%d", x, y, left_button);
-            fflush(stdout); // Para forçar a saída ser exibida imediatamente
+        printf("\rPosição X: %d, Posição Y: %d, Botões: Left=%d", x, y, left_button);
+        fflush(stdout); // Para forçar a saída ser exibida imediatamente
 
 
     }
