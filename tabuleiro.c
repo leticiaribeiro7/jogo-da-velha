6f8835be62bffa5b572f123b16e8327b7b037361
vitoriatanan <vitoriatanans@gmail.com>
2024-04-16 21:12:16 -0300

    Update tabuleiro.c

--- tabuleiro.c
+++ tabuleiro.c
@@ -2,7 +2,8 @@
 
 void imprimirTabuleiro(char tabuleiro[3][3]) {
     printf("\n");
-    for (int i = 0; i < 3; i++) {
+    int i;
+    for (i = 0; i < 3; i++) {
         printf(" %c | %c | %c ", tabuleiro[i][0], tabuleiro[i][1], tabuleiro[i][2]);
         if (i != 2) {
             printf("\n---|---|---\n");
@@ -19,22 +20,13 @@ void imprimirTabuleiro(char tabuleiro[3][3]) {
  *                     caso contrário, retorna 0.
 */
 int verificaDiagonalPrincipal(char tabuleiro[3][3]) {
-    char elemento = tabuleiro[0][0];
-    int contador = 0;
-
-    for (int col = 0; col < 3; col++) {
-        for (int lin = 0; lin < 3; lin++) {
-            if(col == lin && tabuleiro[col][lin] == elemento) {
-                contador++;
-                if(contador == 3) {
-                    printf("Ganhou");
-                    return 1;
-                }
-            }
-        }
-    }
-    //printf("NAO GANHOU");
-    return 0;
+	char elemento = tabuleiro[0][0];
+	for (int i = 0; i < 3; i++) {
+		if (tabuleiro[i][i] != elemento) {
+			return 0; 
+		}
+	}
+	return 1; 
 }
 
 /**
@@ -46,38 +38,84 @@ int verificaDiagonalPrincipal(char tabuleiro[3][3]) {
 */
 int verificaDiagonalSecundaria(char tabuleiro[3][3]) {
     char elemento = tabuleiro[2][0];
-    int contador = 0;
+	for (int i = 0; i < 3; i++) {
+		if (tabuleiro[i][2 - i] != elemento) {
+            //printf(" NAO GANHOU");
+			return 0; 
+		}
+	}
+    //printf("GANHOU");
+	return 1; 
+}
 
-    for (int col = 2; col >= 0; col--) {
-        for (int lin = 2; lin >=0; lin--) {
-            if(col == lin && tabuleiro[col][lin] == elemento) {
-                contador++;
-                
-            }
+/**
+ * Verifica se há um vencedor em uma linha.
+ * 
+ * @param tabuleiro   Matriz de dimensão 3x3
+ * @return            1 se todos os elementos de uma linha forem iguais,
+ *                    caso contrário, retorna 0.
+*/
+int verificarLinhas(char tabuleiro[3][3]) {
+    int lin;
+
+    for (lin = 0; lin < 3; lin++) {
+        if (tabuleiro[lin][0] == tabuleiro[lin][1]  && tabuleiro[lin][1] == tabuleiro[lin][2]) {
+            //printf("GANHOU");
+            return 1;
         }
     }
-    if(contador == 3) {
-        //printf("Ganhou");
+    //printf("NAO GANHOU");
+    return 0;
+}
+
+/**
+ * Verifica se há um vencedor em uma coluna.
+ * 
+ * @param tabuleiro   Matriz de dimensão 3x3
+ * @return            1 se todos os elementos de uma coluna forem iguais,
+ *                    caso contrário, retorna 0.
+*/
+int verificarColunas(char tabuleiro[3][3]) {
+    int col;
+
+    for (col = 0; col < 3; col++) {
+        if (tabuleiro[0][col] == tabuleiro[1][col] && tabuleiro[1][col] == tabuleiro[2][col]) {
+            //printf("GANHOU");
+            return 1;
+        }
+    }
+    //printf("NAO GANHOU");
+    return 0;
+}
+
+int verificarEmpate(char tabuleiro[3][3]) {
+    int resultadoDiagonalPrincipal = verificaDiagonalPrincipal(tabuleiro);
+    int resultadoDiagonalSecundaria = verificaDiagonalSecundaria(tabuleiro);
+    int resultadoLinhas = verificarLinhas(tabuleiro);
+    int resultadoColunas = verificarColunas(tabuleiro);
+
+    if (!resultadoDiagonalPrincipal && !resultadoDiagonalSecundaria && !resultadoLinhas && !resultadoColunas) {
+        //printf("EMPATE!");
         return 1;
     }
     else {
-        //printf("NAO GANHOU");
         return 0;
     }
 }
 
 int main() {
     char tabuleiro[3][3] = {
-        {'O', 'O', 'O'},
-        {'O', 'X', 'X'},
-        {'O', 'O', 'O'}
+        {'X', 'X', 'O'},
+        {'O', 'O', 'X'},
+        {'X', 'O', 'O'}
     };
     imprimirTabuleiro(tabuleiro);
 
     //verificaDiagonalPrincipal(tabuleiro);
-    verificaDiagonalSecundaria(tabuleiro);
-
+    //verificaDiagonalSecundaria(tabuleiro);
+    //verificarLinhas(tabuleiro);
+    //verificarColunas(tabuleiro);
+    //verificarEmpate(tabuleiro);
     return 0;
     
 }
-
