9bf4a57a30141d7a4e810d2495ff2645aa827812
Leticia Ribeiro <68934064+leticiaribeiro7@users.noreply.github.com>
2024-04-25 17:06:15 -0300

    Update tabuleiro.h

--- tabuleiro.h
+++ tabuleiro.h
@@ -4,3 +4,4 @@ int verificarEmpate(char tabuleiro[3][3]);
 int verificarLinhas(char tabuleiro[3][3]);
 int verificarColunas(char tabuleiro[3][3]);
 int verificaDiagonalSecundaria(char tabuleiro[3][3]);
+int verificaTabuleiroVazio(char tabuleiro[3][3]);
