ca773b213d14e041267ab0eb0e39010e9c42fcca
leticiaribeiro7 <letsribeiro2@gmail.com>
2024-05-04 22:34:57 -0300

    simplifying game mechanism

--- main.c
+++ main.c
@@ -22,6 +22,32 @@ int jogadaValida(char tabuleiro[3][3], int i, int j) {
     return 1;
 }
 
+// Limitando coordenadas entre 1 e 200
+void limitarCursor(int *x, int *y) {
+    if (*x <= 1) *x = 1;
+    if (*y <= 1) *y = 1;
+    if (*x >= 200) *x = 200;
+    if (*y >= 200) *y = 200;
+}
+
+int determinarQuadrante(int x, int y) {
+    int quadrante;
+
+    if (x < 50) {
+        quadrante = 1;
+    } else if (x < 100) {
+        quadrante = 2;
+    } else {
+        quadrante = 3;
+    }
+
+    if (y >= 50 && y < 100) {
+        quadrante += 3;
+    } else if (y >= 100) {
+        quadrante += 6;
+    }
+
+    return quadrante;
 }
 
 
@@ -34,12 +60,12 @@ int main() {
     char mouse_buffer[3]; 
     int quadrante = 0;
     char jogador = ' ';
-    int i, j;
     int jogadas = 0;
-    int ultimoEstadoBotao = 0;
+    
+    
 
     fd = open(MOUSEFILE, O_RDONLY); // abre arquivo do mouse
-    KEY_open(); // abre botões
+    //KEY_open(); // abre botões
 
     if (fd == -1) {
         perror("Não é possível abrir o dispositivo do mouse");
@@ -52,10 +78,20 @@ int main() {
         {'7', '8', '9'}
     };
 
+    // Mapeamento do quadrante para as posições do tabuleiro
+    int posicoes[9][2] = {
+        {0, 0}, {0, 1}, {0, 2},
+        {1, 0}, {1, 1}, {1, 2},
+        {2, 0}, {2, 1}, {2, 2}
+    };
+
     while (1) {
        
         if (read(fd, &mouse_buffer, sizeof(mouse_buffer)) > 0) {
+            
             system("clear");
+            printf("===== JOGO DA VELHA =====\n\n");
+            printf("Jogadas: %d\n", jogadas);
             imprimirTabuleiro(tabuleiro);
 
             x_disp = mouse_buffer[1];
@@ -68,77 +104,24 @@ int main() {
             // if (leftButton) { // Verifica se o bit 0 (botão esquerdo) está definido
             //     printf("Clicou em x=%d e y=%d\n", x, y);
             // }
-            // printf("Botão: %d, Posição X: %d, Posição Y: %d\n", button, x, y);
+            //printf("Botão: %d, Posição X: %d, Posição Y: %d\n", button, x, y);
 
+            printf("Vez do jogador %c\n\n", jogador);
+            
+            quadrante = determinarQuadrante(x, y);
             printf("Você está no quadrante %d\n", quadrante);
 
-            jogador = alternaJogadores(leftButton, jogadas);    
 
+            int i = posicoes[quadrante-1][0];
+            int j = posicoes[quadrante-1][1];
 
-            // atualiza quadrante e faz a jogada
-            if (x < 50 ) {
-                if (y < 50) {
-                    quadrante = 1;
-                    if (leftButton && jogadaValida(tabuleiro, 0, 0)) {
-                        tabuleiro[0][0] = jogador;
-                        jogadas++;
-                    }
-                } else if (y > 51 && y < 100) {
-                    quadrante = 4;
-                    if(leftButton && jogadaValida(tabuleiro, 1, 0)) {
-                        tabuleiro[1][0] = jogador;
-                        jogadas++;
-                    }
-                } else if (y > 101) {
-                    quadrante = 7;
-                    if(leftButton && jogadaValida(tabuleiro, 2, 0)) {
-                        tabuleiro[2][0] = jogador;
-                        jogadas++;
-                    }
-                }
-            } else if (x > 51 && x < 100) {
-                if (y < 50) {
-                    quadrante = 2;
-                    if(leftButton && jogadaValida(tabuleiro, 0, 1)) {
-                        tabuleiro[0][1] = jogador;
-                        jogadas++;
-                    }
-                } else if (y > 51 && y < 100) {
-                    quadrante = 5;
-                    if(leftButton && jogadaValida(tabuleiro, 1, 1)) {
-                        tabuleiro[1][1] = jogador;
-                        jogadas++;
-                    }
-                } else if (y > 101) {
-                    quadrante = 8;
-                    if(leftButton && jogadaValida(tabuleiro, 2, 1)) {
-                        tabuleiro[2][1] = jogador;
-                        jogadas++;
-                    }
-                }
-            } else if (x > 101 && x < 200) {
-                if(y < 50) {
-                    quadrante = 3;
-                    if (leftButton && jogadaValida(tabuleiro, 0, 2)) {
-                        tabuleiro[0][2] = jogador;
-                        jogadas++;
-                    }
-
-                } else if (y > 51 && y < 100) {
-                    quadrante = 6;
-                    if(leftButton && jogadaValida(tabuleiro, 1, 2)) {
-                        tabuleiro[1][2] = jogador;
-                        jogadas++;
-                    }
-                } else if (y > 101) {
-                    quadrante = 9;
-                    if(leftButton && jogadaValida(tabuleiro, 2, 2)) {
-                        tabuleiro[2][2] = jogador;
-                        jogadas++;
-                    }
-                }
+            if (leftButton && jogadaValida(tabuleiro, i, j)) {
+                tabuleiro[i][j] = jogador;
+                jogadas++;
             }
         }
+
+        limitarCursor(&x, &y);
 
         int vitoria = verificarVitoria(tabuleiro);
         int empate = verificarEmpate(tabuleiro);
@@ -149,13 +132,18 @@ int main() {
             printf("%c Ganhou!\n", jogador);
             break;
             
-        } else if(empate && jogadas == 9) {
-            printf("Empate\n");
+        } else if (empate && jogadas == 9) {
+            system("clear");
+            imprimirTabuleiro(tabuleiro);
+            printf("Empate!\n");
+            break;
         }
 
-        printf("Jogadas: %d", jogadas);
+        jogador = alternaJogadores(leftButton, jogadas); 
+
         usleep(2000); // Espera 20ms antes de verificar novamente o mouse
     }
+
 
     close(fd);
     return 0;
